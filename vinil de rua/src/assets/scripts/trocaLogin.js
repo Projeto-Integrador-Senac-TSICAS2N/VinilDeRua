56ddbe4855fb1ba150df9acab46d31f79ef9b986
DuduMProg <contatonemai1945@gmail.com>
2025-09-07 20:03:03 -0300

    Add files via upload

--- vinil de rua/src/assets/scripts/trocaLogin.js
+++ vinil de rua/src/assets/scripts/trocaLogin.js
@@ -1,7 +1,16 @@
 const container = document.querySelector('.form-container');
+
 function showForgot() {
   container.classList.add('show-forgot');
+  container.classList.remove('show-criarConta');
 }
+
 function showLogin() {
+  container.classList.remove('show-forgot');
+  container.classList.remove('show-criarConta');
+}
+
+function showCriarConta() {
+  container.classList.add('show-criarConta');
   container.classList.remove('show-forgot');
 }
