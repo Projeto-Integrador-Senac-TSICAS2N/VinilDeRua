5501fd98a99a31b198dcd89f4918cca28b3febba
DuduMProg <contatonemai1945@gmail.com>
2025-09-08 22:11:19 -0300

    Add files via upload

--- vinil de rua/src/assets/scripts/navbar.js
+++ vinil de rua/src/assets/scripts/navbar.js
@@ -1,4 +1,4 @@
 window.addEventListener("scroll", function () {
-    const header = document.querySelector("header");
-    header.classList.toggle("scrolled", window.scrollY > 1);
-  });
+  const header = document.querySelector("header");
+  header.classList.toggle("scrolled", window.scrollY > 1);
+});
