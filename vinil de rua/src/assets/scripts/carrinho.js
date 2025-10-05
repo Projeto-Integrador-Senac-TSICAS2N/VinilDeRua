03d3c0c34e5856fe648d6df965acb2175bf5e301
DuduMProg <contatonemai1945@gmail.com>
2025-10-05 01:35:52 -0300

    Add files via upload

--- vinil de rua/src/assets/scripts/carrinho.js
+++ vinil de rua/src/assets/scripts/carrinho.js
@@ -1,14 +1,33 @@
+
+// Remover produto do carrinho
 const removerDoCarrinho = document.getElementsByClassName("deleteItem")
 for (var i = 0; i < removerDoCarrinho.length; i++){
     removerDoCarrinho[i].addEventListener("click", function(event){
         event.target.parentElement.parentElement.remove()
+        atualizarPreco()
     })
 }
 
-const produtosCarrinho = document.getElementsByClassName("itemCar")
-for (var i = 0; i < produtosCarrinho.length; i++){
-    // console.log(produtosCarrinho[i])
-    const precoFinal = produtosCarrinho[i].getElementsByClassName("infoPrecoF")[0].innerText
-    const quantidadeProduto = produtosCarrinho[i].getElementsByClassName("infoQuantidade")[0].innerText
-    console.log(quantidadeProduto)
+
+function atualizarPreco(){
+    // Calculo de quantidade para o valor final
+    let precoTotal = 0
+    const produtosCarrinho = document.getElementsByClassName("itemCar")
+    for (var i = 0; i < produtosCarrinho.length; i++){
+        // console.log(produtosCarrinho[i])
+        const precoFinal = produtosCarrinho[i].getElementsByClassName("infoPrecoF")[0].innerText.replace("Preço final", "").replace("R$", "").replace(",", ".")
+        // console.log(precoFinal)
+        const quantidadeProduto = produtosCarrinho[i].getElementsByClassName("inputQtd")[0].value
+        // console.log(quantidadeProduto)
+    
+        precoTotal = precoTotal + (precoFinal*quantidadeProduto)
+    
+        // OU precoTotal += precoFinal*quantidadeProduto
+    }
+    // console.log(precoTotal)
+    
+    precoTotal = precoTotal.toFixed(2)
+    precoTotal = precoTotal.replace(".", ",")
+    document.getElementsByClassName("resultadoFinal")[0].innerText = "R$" + precoTotal;
+
 }
